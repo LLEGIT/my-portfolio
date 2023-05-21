75128e3319b1054528f1adb80f378310411622a2
LLEGIT <theogillet.bordeaux@outlook.com>
2023-05-21 15:46:56 +0200

    feat: added menu links and dark theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import { Grid } from '@mui/material';
+import { Grid, ThemeProvider, createTheme } from '@mui/material';
 import Navbar from './components/Navbar/Navbar';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const lightTheme = createTheme({
+    palette: {
+      mode: "light"
+    }
+  })  
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark"
+    }
+  })
+  const [chosenTheme, setChosenTheme] = useState(sessionStorage.getItem("theme") === "dark" ? darkTheme : lightTheme);
+
+  const handleChosenTheme = (theme) => {
+    setChosenTheme(theme === "dark" ? darkTheme : lightTheme);
+  }
+
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Navbar />
+    <ThemeProvider theme={chosenTheme}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Navbar onThemeChange={handleChosenTheme} />
+        </Grid>
       </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,25 @@
-import { AppBar, Box, IconButton, Menu, MenuItem, Select, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, IconButton, Link, Menu, MenuItem, Select, Toolbar, Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import frTranslations from "./../../translations/fr.json";
 import enTranslations from "./../../translations/en.json";
 import { useState } from "react";
-import { Flag } from "@mui/icons-material";
+import { Brightness2, Flag, GitHub, LightMode, LinkedIn, Mail } from "@mui/icons-material";
 
-export default function Navbar() {
+export default function Navbar({ onThemeChange }) {
     const [translations, setTranslations] = useState(sessionStorage.getItem("translation") === "fr" ? frTranslations : enTranslations);
     const [anchorElOne, setAnchorElOne] = useState(null);
     const [anchorElTwo, setAnchorElTwo] = useState(null);
+    const links = [
+        ["#bio", translations.navbar.bio],
+        ["#timeline", translations.navbar.timeline],
+        ["https://www.linkedin.com/in/theogillet/", <LinkedIn />],
+        ["https://github.com/LLEGIT", <GitHub />],
+        ["mailto:theogillet.developpement@gmail.com", <Mail />]
+    ];
+    const [chosenTheme, setChosenTheme] = useState(sessionStorage.getItem("theme") ?? "light");
 
     const handleMenu = (event) => {
-        if (event.target.id === "menu-btn-1") {
+        if (event.currentTarget.id === "menu-btn-1") {
             setAnchorElOne(event.currentTarget);
         }
         else {
@@ -30,6 +38,11 @@ export default function Navbar() {
         setAnchorElTwo(null);
     };
 
+    const handleTheme = () => {
+        sessionStorage.setItem("theme", chosenTheme === "dark" ? "light" : "dark");
+        onThemeChange(chosenTheme === "dark" ? "light" : "dark");
+        setChosenTheme(chosenTheme === "dark" ? "light" : "dark");
+    }
 
     return (
         <Box flexGrow={1}>
@@ -41,6 +54,7 @@ export default function Navbar() {
                         edge="start"
                         color="inherit"
                         aria-label="menu"
+                        onClick={handleMenu}
                         sx={{ mr: 2, display: { lg: "none" } }}>
                         <MenuIcon />
                     </IconButton>
@@ -58,11 +72,22 @@ export default function Navbar() {
                         open={Boolean(anchorElOne)}
                         onClose={handleClose}
                     >
-
+                        {links.map((link, key) => <MenuItem key={key} onClick={handleClose}>
+                            <Link underline="none" target="_blank" color="inherit" href={link[0]}>
+                                {link[1]}
+                            </Link>
+                        </MenuItem>)}
                     </Menu>
                     <Typography display={{xs: "none", lg: "block"}}  variant="h6" component="div" flexGrow={1}>
                         { translations.navbar.title }
                     </Typography>
+                    <IconButton
+                        onClick={handleTheme}
+                        aria-label="change display theme"
+                        color="inherit"
+                    >
+                        {chosenTheme === "light" ? <LightMode /> : <Brightness2 />}
+                    </IconButton>
                     <IconButton
                         id="menu-btn-2"
                         size="large"
