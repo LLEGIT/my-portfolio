3a88feed77142c228f06f261f7c07fe1c9e9ace3
LLEGIT <theogillet.bordeaux@outlook.com>
2023-05-21 17:55:59 +0200

    feat: added image and stack section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Grid, Link, ThemeProvider, Typography, createTheme } from '@mui/material';
+import { Box, Grid, Link, ThemeProvider, Typography, createTheme } from '@mui/material';
 import Navbar from './components/Navbar/Navbar';
 import { useEffect, useState } from 'react';
 import frTranslations from "./translations/fr.json";
 import enTranslations from "./translations/en.json";
 import { GitHub, LinkedIn, Mail } from '@mui/icons-material';
+import HtmlCssJsLogo from "./assets/images/html_css_js.png";
+import MongoDbLogo from "./assets/images/mongodb.png";
+import NestJsLogo from "./assets/images/nestjs.png";
+import ReactLogo from "./assets/images/react.png";
+import SqlLogo from "./assets/images/sql.webp";
+import SymfonyLogo from "./assets/images/symfony.png";
+import TwigLogo from "./assets/images/twig.png";
+import MuiLogo from "./assets/images/mui.png";
+import BootstrapLogo from "./assets/images/bootstrap.png";
+import SassLogo from "./assets/images/sass.png";
 
 function App() {
   const lightTheme = createTheme({
@@ -21,15 +31,16 @@ function App() {
   })
   const [chosenTheme, setChosenTheme] = useState(sessionStorage.getItem("theme") === "dark" ? darkTheme : lightTheme);
   const [translations, setTranslations] = useState(sessionStorage.getItem("translation") === "en" ? enTranslations : frTranslations);
-  const stackIcons = [
-    ["Full front", []],
-    ["MNRN", []],
-    ["Symfony SQL", []]
+  const stacks = [
+    ["Full front", [HtmlCssJsLogo]],
+    ["Mongo Nest React", [MongoDbLogo, NestJsLogo, ReactLogo]],
+    ["Symfony SQL", [SqlLogo, SymfonyLogo, TwigLogo]],
+    ["Style", [MuiLogo, BootstrapLogo, SassLogo]]
   ];
   const mobileLinks = [
-    ["https://www.linkedin.com/in/theogillet/", <LinkedIn />],
-    ["https://github.com/LLEGIT", <GitHub />],
-    ["mailto:theogillet.developpement@gmail.com", <Mail />]
+    ["https://www.linkedin.com/in/theogillet/", <LinkedIn fontSize="large" />],
+    ["https://github.com/LLEGIT", <GitHub fontSize="large" />],
+    ["mailto:theogillet.developpement@gmail.com", <Mail fontSize="large" />]
   ];
 
   const handleChosenTheme = (theme) => {
@@ -55,7 +66,7 @@ function App() {
         <Grid container padding={{xs: 5, lg: 0}} gap={3}>
           {/* Mobile links */}
           <Grid item xs={12} justifyContent="space-around" sx={{display: {xs: "flex", lg: "none"}}}>
-            {mobileLinks.map((link, key) => <Link color="inherit" key={key} target="_blank" underline="none" href={link[0]}>
+            {mobileLinks.map((link, key) => <Link color="inherit" key={key} target={key === 2 ? "_self" : "_blank"} underline="none" href={link[0]}>
               {link[1]}
             </Link>)}
           </Grid>
@@ -73,10 +84,30 @@ function App() {
             </Grid>
           </Grid>
           {/* Stack section */}
-          <Grid container>
+          <Grid container gap={3}>
             <Typography variant="h5" fontWeight="bold">
               {translations.stack.title}
             </Typography>
+            <Grid container gap={2}>
+              {stacks.map((stack, parentKey) => <Grid container gap={1} key={parentKey}>
+                <Grid item xs={12}>
+                  <Typography variant="body1" fontStyle="italic">
+                    {stack[0]}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} display="flex" justifyContent="space-around" alignItems="center">
+                  {stack[1].map((image, childKey) => <Box sx={{width: {xs: parentKey === 0 ? 280 : 75, lg: parentKey === 0 ? 350 : 175}}} key={childKey}>
+                    <img
+                      src={image}
+                      alt="technology logos"
+                      loading="lazy"
+                      width="100%"
+                      height="auto"
+                    />
+                  </Box>)}
+                </Grid>
+              </Grid>)}
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
